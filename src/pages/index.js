9b28e861a45fe462fd84c574b96c805d325a95c7
Mushfiqur Niloy <mushfiqur06@gmail.com>
2019-10-31 13:05:58 +0600

    Remove Image Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,6 @@ import { Link } from "gatsby"
 import { Container, Row } from 'react-bootstrap'
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
