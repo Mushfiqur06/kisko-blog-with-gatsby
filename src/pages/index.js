918e8f1c337e685c7c85cd67ed0a7e8dea853641
Mushfiqur Niloy <mushfiqur06@gmail.com>
2019-11-03 22:33:43 +0600

    Create Blog layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,70 @@
 import React from "react"
 import { Link } from "gatsby"
-import { Container, Row } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import '../sass/index.scss'
+
+import blogImageOne from '../images/post-thumb-md-a.jpg'
 
 const IndexPage = () => (
   <Layout>
-      <Container>
-        <Row md={4}>
-          <h1>Hi people</h1>
-          <p>Welcome to your new Gatsby site.</p>
-          <p>Now go build something great.</p>
-          <SEO title="Home" />
-          <Link to="/page-2/">Go to page 2</Link>
-        </Row>
-      </Container>
+      <section className="blogSection">
+        <Container>
+          <Row>
+            <Col lg={6}>
+              <div className="blogSection__wrapper">
+
+                <div className="blogSection__img">
+                  <Link>
+                    <img src={blogImageOne} alt="" />
+                  </Link>
+                </div>
+
+                <div className="blogSection__body">
+                  <div className="blogSection__header">
+                    <p className="blogSection__header--postDate"><FontAwesomeIcon icon={['far', 'clock']} /> 30 June, 2019</p>
+                    <p className="blogSection__header--authorName"><FontAwesomeIcon icon={['far', 'user']} /> Mushfiqur Niloy</p>
+                    
+                  </div>
+                  <div className="blogSection__content">
+                    <h2 className="blogSection__content--title">
+                      <Link>Subline of her own road the headline of Alphabet Village arround</Link>
+                    </h2>
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about</p>
+                  </div>
+                </div>
+              </div>
+            </Col>
+
+            <Col lg={6}>
+              <div className="blogSection__wrapper">
+
+                <div className="blogSection__img">
+                  <Link>
+                    <img src={blogImageOne} alt="" />
+                  </Link>
+                </div>
+
+                <div className="blogSection__body">
+                  <div className="blogSection__header">
+                    <p className="blogSection__header--postDate"><FontAwesomeIcon icon={['far', 'clock']} /> 30 June, 2019</p>
+                    <p className="blogSection__header--authorName"><FontAwesomeIcon icon={['far', 'user']} /> Mushfiqur Niloy</p>
+                    
+                  </div>
+                  <div className="blogSection__content">
+                    <h2 className="blogSection__content--title">
+                      <Link>Subline of her own road the headline of Alphabet Village arround</Link>
+                    </h2>
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about</p>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
   </Layout>
 )
 
